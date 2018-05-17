236f2777dbe172777c5473d0125f4ea7ac5dd12f
duanebenson-SC <dbenson@screamingcircuits.com>
2018-05-17 10:00:02 -0700

    Add files via upload

---
+++ Test_all_three_colors.ino
@@ -0,0 +1,89 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+// Revised by Duane Benson, 2018
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            9
+
+#define ambientSense A3         // Analog input used to read the amient light value
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      12
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 50; // delay for half a second
+     // variables for light reading
+int sensorValue = 0;             // variable to store the value coming from the sensor
+int stripBright = 0;             // variablw to store adjusted brightness value
+uint8_t  ambient = 32;           // Default brightness level. Will be adjusted by the ambient light sensor
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+  Serial.begin(9600);
+  autoBrightness();              // Check ambient light sensor and adjust the brightness value
+
+  pixels.begin(); // This initializes the NeoPixel library.
+}
+
+void loop() {
+
+  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+
+  for(int i=0;i<NUMPIXELS;i++){
+    autoBrightness();
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(150,0,0)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+    delay(delayval); // Delay for a period of time (in milliseconds).
+
+  }
+  delay(500);
+  for(int i=0;i<NUMPIXELS;i++){
+    autoBrightness();
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+    delay(delayval); // Delay for a period of time (in milliseconds).
+
+  }
+  delay(500);
+  for(int i=0;i<NUMPIXELS;i++){
+    autoBrightness();
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,0,150)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+    delay(delayval); // Delay for a period of time (in milliseconds).
+
+  }
+  delay(500);
+}
+
+//*********************************************************************
+  void autoBrightness() {
+      sensorValue = analogRead(ambientSense);
+      stripBright = map(sensorValue, 0, 1023, 0, 127);
+      Serial.println(sensorValue);
+      stripBright = stripBright + ambient;
+      pixels.setBrightness(stripBright); // set brightness
+  }
+
